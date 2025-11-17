3bd0a0af96205614b5b89dc7f0e0be5842fc12f3
Laura <laura93o@hotmail.com>
2025-11-17 19:44:27 +0100

    commit añadido libreria Lombok

--- src/main/java/com/laura/ficheros/models/Alumno.java
+++ src/main/java/com/laura/ficheros/models/Alumno.java
@@ -1,77 +1,41 @@
+
 package com.laura.ficheros.models;
 
-import jakarta.xml.bind.annotation.*;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
-//alumn@ (expediente, nombre y apellidos).
-//LA SERIALIZACION ES EN BINARIO
-@XmlAccessorType(XmlAccessType.PROPERTY)
+@XmlAccessorType(XmlAccessType.FIELD)
+//@XmlAccessorType(XmlAccessType.PROPERTY)
 @XmlRootElement(name = "Alumno")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Alumno implements Serializable {
-    private String expediente;
-    private String nombre;
-    private String apellidos;
-    private Double nota;
 
+    @XmlElement
+    private String expediente;
+
+    @XmlElement
+    private String nombre;
+
+    @XmlElement
+    private String apellidos;
 
     @XmlElement(name = "nota", type = Double.class)
-    public Double getNota() {
-        return nota;
-    }
+    private Double nota;
 
-    public void setNota(Double nota) {
-        this.nota = nota;
-    }
-
-    public Alumno() {
-        //Constructor vacio requerido por JAXB
-    }
-
-    public Alumno(String expediente, String apellidos, String nombre, Double nota) {
-        this.expediente = expediente;
-        this.apellidos = apellidos;
-        this.nombre = nombre;
-        this.nota = nota;
-    }
-
-    //Constructor con todos los campos
     public Alumno(String expediente, String nombre, String apellidos) {
         this.expediente = expediente;
         this.nombre = nombre;
         this.apellidos = apellidos;
     }
-
-    @XmlElement
-    public String getExpediente() {
-        return expediente;
-    }
-
-    public void setExpediente(String expediente) {
-        this.expediente = expediente;
-    }
-
-    @XmlElement
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-
-    @XmlElement
-    public String getApellidos() {
-        return apellidos;
-    }
-
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
-
 
 
     // Alumno.java (Código corregido para compatibilidad con FicheroTXT/CSV)
@@ -88,5 +52,7 @@ public class Alumno implements Serializable {
                 this.apellidos,
                 notaDisplay);
     }
+
 }
+
 
